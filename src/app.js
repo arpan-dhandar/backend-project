32582072ebe901ac5eafc8facb4690959f0f98bd
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-03-24 08:12:58 +0530

    added register controller and bugs fixed

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-const app =express()
+const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended: true, limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
@@ -21,4 +21,4 @@ import userRouter from './routes/user.routes.js'
 // routes declaration
 app.use("/api/v1/users", userRouter)
 
-export default { app }
+export default app 
